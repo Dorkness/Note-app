e4de3791f8eed2192418f2f54951461c8a6b8236
Dorothy Uji <goodnessuji@gmail.com>
2024-01-05 10:04:33 +0100

    first commit

---
+++ notes-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = ({handleToggleDarkMode}) => {
+    return <div
+    className='header'>
+    <h1>Note Taker</h1>
+    <button onClick= {() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)}  className='save'>Dark Mode</button>
+    </div>;
+}
+
+
+export default Header;
